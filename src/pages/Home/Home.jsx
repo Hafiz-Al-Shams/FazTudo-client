a3c5b136bb699fddba3e2c6ec5d55cfd2f5b11ed
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2025-02-20 17:31:49 +0600

    solved firebase error

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <>
             <div className="text-center">
-                <h2 className="text-2xl md:text-4xl lg:text-5xl text-neutral-800 font-medium pt-12 md:pt-24 lg:pt-40 pb-3.5 md:pb-6 lg:pb-10 px-10">Welcome to FazTudo, Sign In to Continue</h2>
+                <h2 className="text-2xl md:text-4xl lg:text-5xl text-neutral-800 font-medium pt-12 md:pt-24 lg:pt-40 pb-3.5 md:pb-6 lg:pb-10 px-10">Welcome to FazTudo, Sign In to Continue!</h2>
                 <SocialLogin></SocialLogin>
             </div>
         </>
