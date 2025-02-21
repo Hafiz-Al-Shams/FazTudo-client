2cd86bf97aa3851d588b65ad613f4d4b0cef349b
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2025-02-21 12:31:47 +0600

    fixed axios issues for mobile

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosPublic = axios.create({
     // baseURL: 'https://faz-tudo-server.vercel.app'
-    baseURL: 'http://localhost:5000'
+    baseURL: 'https://faz-tudo-server.vercel.app'
 })
 
 const useAxiosPublic = () => {
